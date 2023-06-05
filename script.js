ecc0d4b581d29217046ba3a82e9af05021737a1f
RishikeshPatil20 <rishiofficial1820@gmail.com>
2023-06-05 21:14:26 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const turnOn=()=>{
+    //bulb
+    let imgOb = document.getElementById("bulb");
+    imgOb.setAttribute('src','./images/ONbulb.jpg');
+
+};
+const turnOff =()=>{
+    let imgOb = document.getElementById("bulb");
+    imgOb.setAttribute('src','./images/OFFbulb.jpg');
+};
